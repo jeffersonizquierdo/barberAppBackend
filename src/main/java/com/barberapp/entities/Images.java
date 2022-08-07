80ca007352d385a6c39a453c15586ac5db306d63
Camilo Velasquez hurtado <velasquezhurtadoc@gmail.com>
2022-08-06 23:54:00 -0500

    api hairstyle terminada

--- src/main/java/com/barberapp/entities/Images.java
+++ src/main/java/com/barberapp/entities/Images.java
@@ -26,4 +26,51 @@ public class Images implements Serializable {
 	
 	@Column(length = 200)
 	private String url;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Images() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Images(Long id, String description, String name, String url) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.name = name;
+		this.url = url;
+	}
+	
+	
 }
